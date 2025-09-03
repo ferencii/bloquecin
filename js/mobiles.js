ef3fcef49001cb01e88e7745647f1169f0beb612
ferencii <ferencithibor@gmail.com>
2025-09-03 14:07:37 +0200

    solucionado checkbox de flags IRV off etc

--- js/mobiles.js
+++ js/mobiles.js
@@ -148,8 +148,10 @@ export function generateMobilesSection() {
         section += `${mob.querySelector('.mob-look-desc').value}~\n`;
         section += `${mob.querySelector('.mob-race').value}~\n`;
 
-        const actFlags = getFlagString(mob, 'fieldset[legend="Act Flags"] input[type="checkbox"]');
-        const afectFlags = getFlagString(mob, 'fieldset[legend="Afect Flags"] input[type="checkbox"]');
+        //const actFlags = getFlagString(mob, 'fieldset[legend="Act Flags"] input[type="checkbox"]');
+        //const afectFlags = getFlagString(mob, 'fieldset[legend="Afect Flags"] input[type="checkbox"]');
+        const actFlags = getFlagString(mob, 'Act Flags');
+        const afectFlags = getFlagString(mob, 'Afect Flags');
         section += `${actFlags} ${afectFlags} ${mob.querySelector('.mob-alignment').value} ${mob.querySelector('.mob-group').value}\n`;
 
         section += `${mob.querySelector('.mob-level').value} ${mob.querySelector('.mob-hitroll').value} `;
@@ -160,16 +162,20 @@ export function generateMobilesSection() {
 
         section += `${mob.querySelector('.mob-ac-pierce').value} ${mob.querySelector('.mob-ac-bash').value} ${mob.querySelector('.mob-ac-slash').value} ${mob.querySelector('.mob-ac-exotic').value} ${mob.querySelector('.mob-ac-magic').value}\n`;
 
-        const offFlags = getFlagString(mob, 'fieldset[legend="Ofensivo Flags"] input[type="checkbox"]');
-        const immFlags = getFlagString(mob, 'fieldset[legend="Inmunidades"] input[type="checkbox"]');
-        const resFlags = getFlagString(mob, 'fieldset[legend="Resistencias"] input[type="checkbox"]');
-        const vulnFlags = getFlagString(mob, 'fieldset[legend="Vulnerabilidades"] input[type="checkbox"]');
+        //const offFlags = getFlagString(mob, 'fieldset[legend="Ofensivo Flags"] input[type="checkbox"]');
+        //const immFlags = getFlagString(mob, 'fieldset[legend="Inmunidades"] input[type="checkbox"]');
+        //const resFlags = getFlagString(mob, 'fieldset[legend="Resistencias"] input[type="checkbox"]');
+        //const vulnFlags = getFlagString(mob, 'fieldset[legend="Vulnerabilidades"] input[type="checkbox"]');
+        const offFlags = getFlagString(mob, 'Ofensivo Flags');
+        const immFlags = getFlagString(mob, 'Inmunidades');
+        const resFlags = getFlagString(mob, 'Resistencias');
+        const vulnFlags = getFlagString(mob, 'Vulnerabilidades');
         section += `${offFlags} ${immFlags} ${resFlags} ${vulnFlags}\n`;
 
         section += `${mob.querySelector('.mob-start-pos').value} ${mob.querySelector('.mob-default-pos').value} ${mob.querySelector('.mob-sex').value} ${mob.querySelector('.mob-gold').value}\n`;
 
-        const formFlags = getFlagString(mob, 'fieldset[legend="Forma"] input[type="checkbox"]');
-        const partFlags = getFlagString(mob, 'fieldset[legend="Partes"] input[type="checkbox"]');
+        const formFlags = getFlagString(mob, 'Forma');
+        const partFlags = getFlagString(mob, 'Partes');
         section += `${formFlags} ${partFlags} ${mob.querySelector('.mob-size').value} ${mob.querySelector('.mob-material').value}\n`;
     });
     section += '#0\n\n';
