146cf51eb457adf3adc9d67bde29d2df803d1cc4
ferencii <ferencithibor@gmail.com>
2025-09-03 11:44:04 +0200

    up

--- js/mobiles.js
+++ js/mobiles.js
@@ -144,7 +144,7 @@ export function generateMobilesSection() {
         section += `#${mob.querySelector('.mob-vnum').value}\n`;
         section += `${mob.querySelector('.mob-keywords').value}~\n`;
         section += `${mob.querySelector('.mob-short-desc').value}~\n`;
-        section += `${mob.querySelector('.mob-long-desc').value.replace(/\n/g, ' ')}~\n`;
+        section += `${mob.querySelector('.mob-long-desc').value.replace(/\n/g, ' ')}\n~\n`;
         section += `${mob.querySelector('.mob-look-desc').value}~\n`;
         section += `${mob.querySelector('.mob-race').value}~\n`;
 
